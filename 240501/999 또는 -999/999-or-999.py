7f8b0c7f2cc022af253697540dd74b2d0b298bf8
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-05-01 02:09:36 +0900

    Title: 999 또는 -999, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/999 또는 -999/999-or-999.py
+++ 240501/999 또는 -999/999-or-999.py
@@ -2,13 +2,8 @@ number = list(map(int,input().split()))
 
 for i in range(len(number)):
     
-    if 999 in number:
+    if 999 or -999 in number:
         number.sort
         number.pop()
-        print(max(number), min(number))
-        break
-    if -999 in number:
-        umber.sort
-        number = number[1:]
         print(max(number), min(number))
         break
