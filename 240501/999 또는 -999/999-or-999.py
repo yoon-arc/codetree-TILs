871b126aa42d6d824d1409fda03855404c0249d5
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-05-01 02:08:01 +0900

    Title: 999 또는 -999, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/999 또는 -999/999-or-999.py
+++ 240501/999 또는 -999/999-or-999.py
@@ -3,10 +3,12 @@ number = list(map(int,input().split()))
 for i in range(len(number)):
     
     if 999 in number:
+        number.sort
         number.pop()
         print(max(number), min(number))
         break
     if -999 in number:
+        umber.sort
         number = number[1:]
         print(max(number), min(number))
         break
