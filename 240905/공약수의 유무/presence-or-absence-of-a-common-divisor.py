a23667971543bf2f3002dbcebb5c32d671329fcb
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-09-05 16:40:24 +0900

    Title: 공약수의 유무, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240905/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+gcd = []
+result = 0
+
+for i in range(1, 2881):
+    if (1920 % i == 0 and 2880 % i == 0):
+        gcd.append(i)
+
+for i in range (a, b+1):
+    if i in gcd:
+        result += 1
+  
+if result:
+    print(1)
+else:
+    print(0)
