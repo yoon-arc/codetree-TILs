c09f870cc9b9161908086f126643f9d2e3223b24
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-09-24 16:52:50 +0900

    Title: palindrome 여부 판단하기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 240924/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,9 +1,8 @@
 def isPalindrome(A):
-    for i in range(1,len(A)//2+1):
-        if A[i-1] == A[-i]:
-            continue
-        else:
-            return 'No'
-    return 'Yes'
+    return A == A[::-1]
+
 A = input()
-print(isPalindrome(A))
+if isPalindrome(A):
+    print('Yes')
+else:
+    print('No')
