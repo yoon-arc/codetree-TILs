32cbc4a7d4cd4d4ae9b6d5f7346cd0f53491e6d8
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-09-26 17:06:05 +0900

    Title: 나누고 빼면서 합하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240926/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 240926/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -2,14 +2,15 @@ n, m = map(int, input().split())
 numList = list(map(int, input().split()))
 
 def rep(M):
-    sum = M
+    sumUp = numList[M-1]
+#     print(M)
     while M != 1:
         if M%2 == 1:
             M -= 1
-            sum += numList[M]
+            sumUp += numList[M-1]
         elif M%2 == 0:
             M //= 2
-            sum += numList[M]
-    return print(sum) 
+            sumUp += numList[M-1]
+    return print(sumUp) 
 
 rep(m)
