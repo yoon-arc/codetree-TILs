8a320160d78763ed77bbbf148b515e31facf20bd
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-08 16:16:15 +0900

    Title: DateTime to DateTime, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 241008/DateTime to DateTime/datetime-to-datetime.py
+++ 241008/DateTime to DateTime/datetime-to-datetime.py
@@ -4,19 +4,20 @@ hour = 11
 minu = 11
 total = 0
 
-while True:
-    if a< 11 and b < 11 and c < 11:
-        print(-1)
-        break
-    if day == a and hour == b and minu == c:
-        print(total)
-        break
+# 목표 시간이 기준 시간보다 앞서는 경우 처리
+if a < 11 or (a == 11 and b < 11) or (a == 11 and b == 11 and c < 11):
+    print(-1)
+else:
+    while True:
+        if day == a and hour == b and minu == c:
+            print(total)
+            break
 
-    if hour == 24:
-        day += 1
-        hour = 0
-    if minu == 60:
-        hour += 1
-        minu = 0
-    total += 1    
-    minu += 1
+        if hour == 24:
+            day += 1
+            hour = 0
+        if minu == 60:
+            hour += 1
+            minu = 0
+        total += 1    
+        minu += 1
