53daae052a302ea82d9c0de1fb9c7e6f8477ceb6
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-09 16:54:02 +0900

    Title: 요일 맞추기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/요일 맞추기/guess-day-of-week.py
+++ 241009/요일 맞추기/guess-day-of-week.py
@@ -2,8 +2,10 @@ m1, d1, m2, d2 = map(int, input().split())
 months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
 
-if  m1 > m2:
+if  m1 > m2 and d1>=d2:
     getDay = ((months[m2] - d2) + sum(months[m2+1:m1]) + d1)
+elif m1 > m2 and d1 < d2:
+    getDay = 7-(((months[m2] - d2) + d1))
 elif m1 == m2 and d2>= d1:
     getDay = d2 - d1
 elif m1 == m2 and d1> d2:
