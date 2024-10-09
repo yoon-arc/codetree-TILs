b6619ab5c64b00cea05880accc25bd095f01ab0c
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-09 17:01:21 +0900

    Title: 요일 맞추기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/요일 맞추기/guess-day-of-week.py
+++ 241009/요일 맞추기/guess-day-of-week.py
@@ -1,18 +1,23 @@
 m1, d1, m2, d2 = map(int, input().split())
 months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-if  m1 > m2 and d1>=d2:
-    getDay = ((months[m2] - d2) + sum(months[m2+1:m1]) + d1)
-elif m1 > m2 and d1 < d2:
-    getDay = 7-(((months[m2] - d2) + d1))
-elif m1 == m2 and d2>= d1:
-    getDay = d2 - d1
-elif m1 == m2 and d1> d2:
-    getDay = 7-(d1 - d2)
-else:
-    getDay = ((months[m1] - d1) + sum(months[m1+1:m2]) + d2)
-    
-getDay %= 7
+# 두 날짜 사이의 일수 차이 계산
+def calculate_day_difference(m1, d1, m2, d2):
+    # 두 날짜가 같은 월일 경우
+    if m1 == m2:
+        return d2 - d1
+    # 두 날짜가 다른 월일 경우
+    elif m1 < m2:
+        return (months[m1] - d1) + sum(months[m1+1:m2]) + d2
+    else:  # m1 > m2
+        return -((months[m2] - d2) + sum(months[m2+1:m1]) + d1)
 
+# 기준 날짜에서 두 번째 날짜까지의 일수 차이
+day_difference = calculate_day_difference(m1, d1, m2, d2)
+
+# 요일 계산 (기준 날짜는 월요일)
+getDay = (day_difference % 7 + 7) % 7  # 음수를 방지하기 위해 + 7
+
+# 결과 출력
 print(days[getDay])
