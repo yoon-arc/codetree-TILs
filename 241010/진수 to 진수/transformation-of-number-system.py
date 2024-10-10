25f038c81e4303228ca7e5e609ccbe6eca500841
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-10 16:38:49 +0900

    Title: 진수 to 진수, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241010/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+n = input()
+# a진수로 표현된 숫자 n
+# b는 바꿔야하는 진수
+
+def getNum(n):
+    count = 0
+    for i in range(len(n)):
+        count += int(n[i])*(a**(len(n)-i-1))
+    return count
+
+def changeNum(c):
+    nums = []
+    while c >= b:
+        now = c%b
+        c = c//2
+        nums.append(now)
+        
+    nums.append(c)
+    return nums[::-1]
+    
+print(*changeNum(getNum(n)), sep="")
