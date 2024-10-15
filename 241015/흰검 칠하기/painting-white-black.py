d319b0d3a61074e952a8d4f20d4e88a789ab57d4
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-15 16:33:10 +0900

    Title: 흰검 칠하기, Time: 353ms, Memory: 32MB, Status: Passed - CodeTree

--- 241015/흰검 칠하기/painting-white-black.py
+++ 241015/흰검 칠하기/painting-white-black.py
@@ -1,32 +1,48 @@
-n = int(input())  # 명령의 개수
-colors = [0] * 1000  # 색상 배열 초기화 ('0'은 아직 색이 결정되지 않음을 의미)
-counts = [0] * 1000  # 카운트 배열 초기화
-moves = [input().split() for _ in range(n)]  # 이동 명령 입력
-now = 500  # 시작 위치 설정
+n = int(input())
 
-for move in moves:
-    steps = int(move[0])  # 이동할 칸 수
-    direction = move[1]   # 이동 방향 ('L' 또는 'R')
+arr = ['N' for _ in range(200001)]
+black_count = [0 for _ in range(200001)]
+white_count = [0 for _ in range(200001)]
 
-    if direction == 'R':
-        # 오른쪽으로 이동
-        for j in range(now, now + steps):
-            counts[j] += 1
-            if counts[j] >= 4:
-                colors[j] = 'G'  # 카운트가 4 이상이면 'G'로 고정
-            elif colors[j] != 'G':  # 'G'가 아닌 경우에만 'B'로 덮음
-                colors[j] = 'B'
-        now += steps  # 현재 위치 업데이트
+cursor = 100000
 
-    elif direction == 'L':
-        # 왼쪽으로 이동
-        for j in range(now - steps, now):
-            counts[j] += 1
-            if counts[j] >= 4:
-                colors[j] = 'G'  # 카운트가 4 이상이면 'G'로 고정
-            elif colors[j] != 'G':  # 'G'가 아닌 경우에만 'W'로 덮음
-                colors[j] = 'W'
-        now -= steps  # 현재 위치 업데이트
+def print_tile(cursor, direction):
+    now = arr[cursor]
+    if direction == 'L':
+        white_count[cursor] += 1
+        if check_gray(cursor):
+            arr[cursor] = 'G'
+        else:
+            arr[cursor] = 'W'
+    else: # 'R'
+        black_count[cursor] += 1
+        if check_gray(cursor):
+            arr[cursor] = 'G'
+        else:
+            arr[cursor] = 'B'
 
-# 'W', 'B', 'G'의 개수 출력
-print(colors.count('W'), colors.count('B'), colors.count('G'))
+def check_gray(cursor):
+    return black_count[cursor] >= 2 and white_count[cursor] >= 2
+
+
+for _ in range(n):
+    distance, direction = input().split()
+
+    for i in range(int(distance)):
+        print_tile(cursor, direction)
+        if direction == 'L':
+            cursor -= 1
+        else:
+            cursor += 1
+    # 제자리에 멈춰야함
+    if direction == 'L':
+        cursor += 1
+    else:
+        cursor -= 1
+
+    
+w = arr.count('W')
+b = arr.count('B')
+g = arr.count('G')
+
+print(w, b, g, sep=" ")
