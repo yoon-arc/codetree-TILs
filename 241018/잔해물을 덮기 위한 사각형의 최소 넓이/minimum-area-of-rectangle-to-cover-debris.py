97ccebcbba8acc4919b886e365c403b841873b07
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-18 18:25:14 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 180ms, Memory: 54MB, Status: Wrong Answer - CodeTree

--- 241018/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 241018/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -2,15 +2,21 @@ offset = 1000
 total = 2001
 checked = [[0]* total for _ in range(total)]
 first = tuple(map(lambda x : x + offset, map(int, input().split())))
-sec = tuple(map(lambda x : x+offset, map(int, input().split())))
+sec = tuple(map(lambda x : x + offset, map(int, input().split())))
 
-for i in range(first[0], first[2]):
-    for j in range(first[1], first[3]):
+for i in range(first[0], first[2]+1):
+    for j in range(first[1], first[3]+1):
         checked[i][j] = 1
-        
-for i in range(sec[0]-first[0], sec[2]-first[2]):
-    for j in range(sec[1]-first[1],sec[3]-first[3]):
-        checked[i][j] = 0
+
+xS = max(first[0], sec[0])
+xE = min(first[2], sec[2])
+yS = max(first[1],sec[1])
+yE = min(first[3],sec[3])
+
+if xS < xE and yS < yE:        
+    for i in range(xS,xE+1):
+        for j in range(yS,yE+1):
+            checked[i][j] = 0
 
 sum = 0
 for i in range(total):
