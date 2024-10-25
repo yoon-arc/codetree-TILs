095e9b568821c00d6560a3c3f7aa95fb5513933d
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-25 15:08:30 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 241025/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241025/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -6,31 +6,22 @@ def getInfection(N, K, P, T):
     infection_log.sort()
     
     for t, x, y in infection_log:
-        # x와 y 중 적어도 한 명이 감염 상태일 경우
-        if developers[x-1] or developers[y-1]:
-            # 둘 중 하나만 감염이 된 상태일 때
-            if developers[x-1] and developers_check[x-1] < K and not developers[y-1]:
-                developers[y-1] = 1
-                developers_check[x-1] += 1
-                developers_check[y-1] += 1
-            elif developers[y-1] and developers_check[y-1] < K and not developers[x-1]:
-                developers[x-1] = 1
-                developers_check[y-1] += 1
-                developers_check[x-1] += 1
+
+        #감염 여부를 확인하고 일단 악수 횟수 증가
+        if developers[x-1]:
+            developers_check[x-1] += 1
+        if developers[y-1]:
+            developers_check[y-1] += 1
+
+        
+        # 둘 중 하나만 감염이 된 상태일 때
+        if developers[x-1] and developers_check[x-1] <= K and not developers[y-1]:
+            developers[y-1] = 1
             
-            # 둘 다 감염 상태이며 아직 횟수가 안 찼을 때
-            elif developers[x-1] and developers[y-1]:
-                if developers_check[x-1] < K:
-                    developers_check[x-1] += 1
-                if developers_check[y-1] < K:
-                    developers_check[y-1] += 1
-            
-            # 둘 다 감염 상태이며 K를 초과한 경우, K만큼만 카운팅
-            elif developers[x-1] and developers_check[x-1] >= K:
-                developers_check[y-1] += 1
-                
-            elif developers[y-1] and developers_check[y-1] >= K:
-                developers_check[x-1] += 1
+        if developers[y-1] and developers_check[y-1] <= K and not developers[x-1]:
+            developers[x-1] = 1
+        
+
 
     print(*developers, sep="")
 
