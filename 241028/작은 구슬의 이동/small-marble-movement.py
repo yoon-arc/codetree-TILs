b25b00f8b16907529ac3ee5e06986d149d676ca8
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-10-28 23:41:39 +0900

    Title: 작은 구슬의 이동, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 241028/작은 구슬의 이동/small-marble-movement.py
+++ 241028/작은 구슬의 이동/small-marble-movement.py
@@ -1,29 +1,24 @@
 n, t  = map(int, input().split()) #행 정보
 r,c,d = input().split() #초기 행, 열, 방향
-nx, ny = int(r), int(c)
+nh, ny = int(r), int(c)
 
 mapper = {'U':0, 'R':1, 'L':2, 'D':3}
-dxs, dys = [0,1,-1,0],[1,0,0,-1]
-dx, dy = dxs[mapper[d]], dys[mapper[d]]
+reoul, hang = [0,1,-1,0],[-1,0,0,1]
+dy, dh = reoul[mapper[d]], hang[mapper[d]]
 
 
 def in_range(x,y):
     return 1<= x <=n and 1<= y <=n
     
 for _ in range(t):# 전체 반복
-    nx += dy
-    ny += dx
+    # print("방향",dh,dy)
+    nh += dh
+    ny += dy
 
-    if not in_range(nx,ny):
-        dx *= -1
+    if not in_range(nh,ny):
+        dh *= -1
         dy *= -1
-        nx += dy
-        ny += dx
+        nh += dh
+        ny += dy
 
-
-    
-    
-            
-
-
-print(nx, ny)
+print(nh, ny)
