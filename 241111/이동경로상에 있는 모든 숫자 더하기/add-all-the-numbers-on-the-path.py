646bff9308971d93e1184021a0829d36571184e7
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-11-11 17:16:38 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 67ms, Memory: 19MB, Status: Passed - CodeTree

--- 241111/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 241111/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -1,23 +1,52 @@
+# N, T 입력 받기
 N, T = map(int, input().split())
 grid = []
+
+# 명령들 입력 받기
 commands = list(input())
+
+# for문 N번만큼, input list
 for _ in range(N):
     grid.append(list(map(int, input().split())))
+
+#방향키
 dhs,dys = [-1,0,1,0],[0,1,0,-1]
+
+#시작 값 설정
 s = (N-1)//2
 h,y = s,s
 d = 0
+
+#범위 내인지 함수
 def in_range(h,y):
     return 0<=h<N and 0<=y<N
+
+#방향 설정 함수
 def get_direction(c):
     return (d-1+4)%4 if c == 'L' else (d+1)%4
-result = 0
+#총합
+result = grid[h][y]
+
+
+#for문(T안에서 돌기)
 for c in commands:    
     if c != 'F' :
         d = get_direction(c)
-    nh, ny = h+dhs[d],y+dys[d]
-    if not in_range(nh,ny):
         continue
-    result += grid[h][y]
-    h, y = nh,ny
+        
+    nh, ny = h+dhs[d],y+dys[d]
+    if in_range(nh,ny):
+        h, y = nh, ny
+        result += grid[h][y]
+        
+
+
+
+    #범위 밖일 시 continue
+
+   
+    # result += grid[h][y]
+
+    
+
 print(result)
