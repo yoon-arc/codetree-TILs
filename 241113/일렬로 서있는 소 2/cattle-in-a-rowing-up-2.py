a79e9f771a148d16b181771a12a98742d5dcdf49
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-11-13 23:03:10 +0900

    Title: 일렬로 서있는 소 2, Time: 54ms, Memory: 17MB, Status: Passed - CodeTree

--- 241113/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 241113/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,12 +1,13 @@
-import itertools
 N = int(input())
 cows = list(map(int, input().split()))
 count = 0
-cowComb = itertools.combinations(cows,3)
 
-for comb in cowComb:
-    A,B,C = comb[0],comb[1],comb[2]
-    if A<=B<=C and cows.index(A)<cows.index(B)<cows.index(C):
-        count +=1
+for a in range(N):
+    for b in range(a+1, N):
+        for c in range(b+1, N):
+            A,B,C = cows[a],cows[b],cows[c]
+            if A<=B<=C:
+                count += 1
+
 
 print(count)
