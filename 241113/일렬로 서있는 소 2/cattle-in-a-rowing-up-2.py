09dde83b28c3aa01720b51ffbb038353999de6e5
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-11-13 17:11:33 +0900

    Title: 일렬로 서있는 소 2, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241113/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,12 @@
+import itertools
+N = int(input())
+cows = list(map(int, input().split()))
+count = 0
+cowComb = itertools.combinations(cows,3)
+
+for comb in cowComb:
+    A,B,C = comb[0],comb[1],comb[2]
+    if A<B<C and cows.index(A)<cows.index(B)<cows.index(C):
+        count +=1
+
+print(count)
