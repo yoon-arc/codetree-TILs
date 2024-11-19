08dc26ea3b6162be7a3d08c6b8c050e6e14e3f18
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-11-19 16:41:41 +0900

    Title: 이상한 진수 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241119/이상한 진수 2/awkward-digits-2.py
+++ 241119/이상한 진수 2/awkward-digits-2.py
@@ -1,8 +1,11 @@
 N = list(input())
 
-for i in range(len(N)):
-    if N[i] != '1':
-        N[i] = '1'
-        break
-print(int(''.join(N),2))
+if N[0] == '0' and len(N) == 1:
+    print(0)
+else:
+    for i in range(len(N)):
+        if N[i] != '1':
+            N[i] = '1'
+            break
+    print(int(''.join(N),2))
         
