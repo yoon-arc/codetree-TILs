c04e85607fb3cd2bff4b318f70de53df0e17aad2
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-11-27 18:02:49 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241127/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,14 @@
+#배열에서 [i]와 [i+1]가 둘 다 == '('인지
+#그렇다면 [i+2]부터 끝까지 돌면서 j와 j+1이 '))'인지
+#맞다면 카운트
+
+braces = list(input())
+count = 0
+#첫 번째 반복문 '(','('를 찾기 위한
+for i in range(len(braces)):
+    if braces[i] == '('and braces[i+1] == '(':
+        for j in range(i+2,len(braces)):
+            if braces[j-1] == ')'and braces[j] == ')':
+                count += 1
+
+print(count)
