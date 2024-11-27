9f18e8834977b99be5b07d4cc5094d1716da2de8
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2024-11-27 20:46:14 +0900

    Title: Carry 피하기 2, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 241127/Carry 피하기 2/escaping-carry-2.py
+++ 241127/Carry 피하기 2/escaping-carry-2.py
@@ -35,4 +35,44 @@ for a,b,c in combinations(nums, 3):
 print(max_num)
         
     
+# 내 원래 코드
+# from itertools import combinations 
+
+# #문자 길이 통일해주는 함수
+# def make_even(n,max_len):
+#     if len(n) < max_len:
+#         n = ('0'*(max_len-len(n))) + n
+#     return n
+
+# #확인하는 함수
+# def is_the_one(a,b,c,max_len):
+#     is_true = True
+#     for i in range(max_len):
+#         if int(a[i])+int(b[i])+int(c[i]) >= 10:
+#             is_true = False
+#     return is_true
+
+
+# #기본 값 받기
+# n = int(input())
+# nums = [input() for _ in range(n)]
+# max_num = -1
+
+# #숫자 확인하기
+# for a,b,c in combinations(nums, 3):
+#     now_num = -100
+#     max_length = max(len(a),len(b),len(c))
+    
+#     #문자 길이 통일
+#     a,b,c = make_even(a,max_length), make_even(b,max_length), make_even(c,max_length)
+#     now_num = int(a)+int(b)+int(c) if is_the_one(a,b,c,max_length) else -100
+   
+#     if now_num > max_num:
+#         max_num = now_num
+        
+# print(max_num) if max_num != -100 else print(-1)
+
+        
+        
+    
     
